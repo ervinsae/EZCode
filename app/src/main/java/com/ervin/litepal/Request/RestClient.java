826b1fe65a86f28da8e0aeeddd75df53ce7e7c6e
Ervin <ervinsae@gmail.com>
2016-03-24 10:53:41 +0800

    1，Retrofit加入请求超时机制

--- app/src/main/java/com/ervin/litepal/Request/RestClient.java
+++ app/src/main/java/com/ervin/litepal/Request/RestClient.java
@@ -1,6 +1,9 @@
 package com.ervin.litepal.request;
 
 import com.ervin.litepal.api.APIService;
+import com.squareup.okhttp.OkHttpClient;
+
+import java.util.concurrent.TimeUnit;
 
 import retrofit.GsonConverterFactory;
 import retrofit.Retrofit;
@@ -11,22 +14,33 @@ import retrofit.RxJavaCallAdapterFactory;
  */
 public class RestClient {
     private static APIService apiService;
+    private static final int DEFAULT_TIMEOUT = 5;
+    static OkHttpClient client;
+
     public RestClient(String url){
 
-        Retrofit retrofit = new Retrofit.Builder().baseUrl(url).addConverterFactory(GsonConverterFactory.create()).build();
+        setTimeOut();
+        Retrofit retrofit = new Retrofit.Builder().client(client).baseUrl(url).addConverterFactory(GsonConverterFactory.create()).build();
 
         apiService = retrofit.create(APIService.class);
     }
 
     public static APIService RestRxClient(String url){
 
-        Retrofit retrofit = new Retrofit.Builder().baseUrl(url).addConverterFactory(GsonConverterFactory.create()).addCallAdapterFactory(RxJavaCallAdapterFactory.create()).build();
+        setTimeOut();
+        Retrofit retrofit = new Retrofit.Builder().client(client).baseUrl(url).addConverterFactory(GsonConverterFactory.create()).addCallAdapterFactory(RxJavaCallAdapterFactory.create()).build();
         apiService = retrofit.create(APIService.class);
         return apiService;
     }
 
     public APIService getApiService(){
         return apiService;
+    }
+
+    public static void setTimeOut(){
+        //手动创建一个OkHttpClient并设置超时时间
+        client = new OkHttpClient();
+        client.setConnectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS);
     }
 
 }
